e608b04d9ece30b18fa1535173e944cec55c812c
lajawanti <lajawanti.dhake@gmail.com>
2021-11-16 14:22:56 -0500

    component structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
 
-function App() {
+import { CssBaseline, Grid } from "@material-ui/core";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>GITHub actions...... with yml</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid
+        container
+        soacing={3}
+        style={{ width: "100%", backgroundColor: "cyan" }}
+      >
+        <Grid item xs={12} md={4} style={{ backgroundColor: "lightPink" }}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8} style={{ backgroundColor: "lightYellow" }}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
